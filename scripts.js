768c8c8f90053198df5fe906374ee37806cc92e7
Nima Ashjaee <63869078+NimaAshjaee75@users.noreply.github.com>
2023-01-21 21:37:04 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,64 @@
+window.onload = function () {
+
+var dataPoints = [];
+
+var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	theme: "light2", // "light1", "light2", "dark1", "dark2"
+	exportEnabled: true,
+	title: {
+		text: "tick"
+	},
+	axisX: {
+		interval: 1,
+		valueFormatString: "MMM"
+	},
+	axisY: {
+         stripLines:[
+            {
+                
+                startValue:90,
+                endValue:100,                
+                color:"#A5FF33",
+            }
+            ],
+		prefix: "$",
+		title: "Price"
+	},
+	// toolTip: {
+	// 	content: "Date: {x}<br /><strong>Price:</strong><br />Open: {y[0]}, Close: {y[3]}<br />High: {y[1]}, Low: {y[2]}"
+	// },
+	data: [{
+		type: "candlestick",
+		yValueFormatString: "$##0.00",
+		dataPoints: dataPoints
+	}]
+});
+
+$.get("https://canvasjs.com/data/gallery/javascript/netflix-stock-price.csv", getDataPointsFromCSV);
+
+function getDataPointsFromCSV(csv) {
+	var csvLines = points = [];
+	csvLines = csv.split(/[\r?\n|\r|\n]+/);
+	for (var i = 0; i < csvLines.length; i++) {
+		if (csvLines[i].length > 0) {
+			points = csvLines[i].split(",");
+			dataPoints.push({
+				x: new Date(
+					parseInt(points[0].split("-")[0]),
+					parseInt(points[0].split("-")[1]),
+					parseInt(points[0].split("-")[2])
+				),
+				y: [
+					parseFloat(points[1]),
+					parseFloat(points[2]),
+					parseFloat(points[3]),
+					parseFloat(points[4])
+				]
+			});
+		}
+	}
+	chart.render();
+}
+
+}
